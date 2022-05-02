30de9e4185327222988a7190f3f53c92a3b7b9e6
Burke Azbill <burkeazbill@users.noreply.github.com>
2022-05-02 13:48:22 -0400

    Update install script to place in preferred bin folder

--- install.sh
+++ install.sh
@@ -1,14 +1,32 @@
 #!env bash
-
-INSTALL_PATH="/usr/local/bin"
-mkdir -p $INSTALL_PATH
 if [ ! -d "MergekubeRemkube" ]; then
   git clone https://github.com/burkeazbill/MergekubeRemkube.git
 fi
 cd MergekubeRemkube
 git pull
 chmod +x mergekube remkube
-sudo cp {mergekube,remkube} $INSTALL_PATH
+
+# Set install path to first matching folder found in path:
+case :$PATH: in
+  *:$HOME/.local/bin:*) 
+    INSTALL_PATH="$HOME/.local/bin"
+  ;; # do nothing, it's there
+  *:$HOME/bin:*) 
+    INSTALL_PATH="$HOME/bin"
+  ;; # do nothing, it's there
+  *:/usr/local/bin:*) 
+    INSTALL_PATH="/usr/local/bin"
+  ;; # do nothing, it's there
+  *) echo "no match found" >&2;;
+esac
+CMD_PREFIX=""
+if [ $INSTALL_PATH == "/usr/local/bin" ]; then
+  CMD_PREFIX="sudo "
+fi
+
+echo "Executing Command: ${CMD_PREFIX}cp {mergekube,remkube} $INSTALL_PATH"
+${CMD_PREFIX}cp {mergekube,remkube} $INSTALL_PATH
+
 cd ..
 # Now confirm they are installed:
 which mergekube
